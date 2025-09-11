096e87767287ba7d7fde85a75697805d42d2d0d1
HelkinJimenezM <165401178+HelkinJimenezM@users.noreply.github.com>
2025-09-11 13:46:59 -0500

    Update TaskDAO.js

--- backend/api/dao/TaskDAO.js
+++ backend/api/dao/TaskDAO.js
@@ -1,10 +1,20 @@
-const User = require("../models/User");
-const GlobalDao = require("./GlobalDao");
+// /dao/taskDao.js
+import Task from "../models/Task.js";
 
-class UserDAO extends GlobalDao {
-    constructor(){
-        super(User);
-    }
-}
+/**
+ * Inserta una nueva tarea en la base de datos
+ * @param {Object} taskData - Datos de la tarea
+ * @returns {Promise<Task>}
+ */
+export const createTaskInDB = async (taskData) => {
+  const task = new Task(taskData);
+  return await task.save();
+};
 
-module.exports = new UserDAO();
+/**
+ * Obtiene todas las tareas de la base de datos
+ * @returns {Promise<Task[]>}
+ */
+export const getAllTasksFromDB = async () => {
+  return await Task.find();
+};
