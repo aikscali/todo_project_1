fa011da8c0f3e9991c86028317eb3f0a95370973
Hj <helkin.jimenez@correounivalle.edu.co>
2025-09-16 22:40:35 -0500

    merge

--- backend/api/dao/TaskDAO.js
+++ backend/api/dao/TaskDAO.js
@@ -1,22 +1,10 @@
-// /dao/taskDao.js
-const Task = require("../models/Task");
+const User = require("../models/Task");
+const GlobalDao = require("./GlobalDao");
 
-/**
- * Inserta una nueva tarea en la base de datos
- * @param {Object} taskData - Datos de la tarea
- * @returns {Promise<Task>}
- */
-const createTaskInDB = async (taskData) => {
-  const task = new Task(taskData);
-  return await task.save();
-};
+class TaskDao extends GlobalDao {
+    constructor(){
+        super(User);
+    }
+}
 
-/**
- * Obtiene todas las tareas de la base de datos
- * @returns {Promise<Task[]>}
- */
-const getAllTasksFromDB = async () => {
-  return await Task.find();
-};
-
-module.exports = { createTaskInDB, getAllTasksFromDB };
+module.exports = new TaskDao();
